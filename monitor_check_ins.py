6d3dbecd903864ffe7c3e1019a6231a07433d452
Taylor Smith <taylor.smith2@uky.edu>
2023-11-09 13:52:12 -0500

    Changed output to dataframe using multi-index instead of resetting before import and added msi_grace as a means of accounting for grace period

--- monitor_check_ins.py
+++ monitor_check_ins.py
@@ -3,47 +3,59 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 import numpy as np
 import pandas as pd
+from dateutil.relativedelta import relativedelta as rdelt
 
 api_url = 'https://redcap.uky.edu/redcap/api/'
 with open("api_key.txt", "r") as f:
     api_key = f.readline().strip()
 project = Project(api_url, api_key)
 date_format = "%Y-%m-%d"
+gp_days = 7                         # Check-in grace period
 
 fields = ['idate', 'chkdate']
 events = ['intake_arm_1', 'monthy_checkin_arm_1']
 data = project.export_records(format_type="df", fields=fields, raw_or_label="raw", events=events)
-data = data.reset_index()
-ids = data['cid'].unique()
+ids = data.index.get_level_values(0).unique()
 
-out = []
+event_name = 'reporting_arm_1'
+index = pd.MultiIndex.from_product([ids, [event_name]], names=['cid', 'redcap_event_name'])
+columns = ["dintake", "msi", "msi_grace", "chktot", "misschk"]
+values = [[""] * len(columns)] * len(ids)
+out = pd.DataFrame(data=values, index=index, columns=columns)
 
 for cid in ids:
-    out.append({'cid': str(cid), 'redcap_event_name': 'reporting_arm_1',
-                'dintake': '', 'msi': '', 'chktot': '', 'misschk': ''})
-    intake = data[np.logical_and(data['cid'] == cid, data['redcap_event_name'] == 'intake_arm_1')]['idate'].values[0]
+    intake = data['idate'][cid]['intake_arm_1']
     if type(intake) != str or intake == "":
-        out = out[:-1]
         ValueError(f"Problem with client id {cid} intake: {intake}")
+        out = out.drop(cid, level=0)
         continue
-    out[-1]['dintake'] = intake
     intake = dt.strptime(intake, date_format)
     today = dt.today()
     msi = ((today.year - intake.year) * 12) + (today.month - intake.month)
-    if today.day < intake.day:
+    if today.day < intake.day:      # only count if the same day if it has reached same day of the month
         msi -= 1
-    check_deadline = dt.strptime(f"{intake.year + (intake.month + msi) // 12}-{(intake.month + msi) % 12}-{intake.day}",
-                                 date_format) + td(days=7)
-    if today <= check_deadline:
-        target_checks = msi - 1
+    check_deadline = intake + rdelt(months=msi, days=gp_days)
+    if today.day >= intake.day:
+        if today <= check_deadline:
+            msi_grace = msi - 1
+        else:
+            msi_grace = msi
     else:
-        target_checks = msi
-    chktot = len(data[np.logical_and(data['cid'] == cid, data['redcap_event_name'] == 'monthy_checkin_arm_1')])
-    misschk = msi - chktot
-    out[-1]['msi'] = str(msi)
-    out[-1]['chktot'] = str(chktot)
-    out[-1]['misschk'] = str(target_checks - chktot)
+        msi_grace = msi
+    try:    # If no monthly checkins, this will throw a KeyError
+        checks = data.loc[cid].loc["monthy_checkin_arm_1"]
+        if len(checks.shape) == 2:
+            chktot = len(data.loc[cid].loc["monthy_checkin_arm_1"])
+        else:
+            chktot = 1
+    except KeyError:
+        chktot = 0
+    misschk = max(msi_grace - chktot, 0)
 
-df = pd.DataFrame.from_records(out)
+    out.loc[(cid, 'reporting_arm_1'), 'dintake'] = dt.strftime(intake, date_format)
+    out.loc[(cid, 'reporting_arm_1'), 'msi'] = str(msi)
+    out.loc[(cid, 'reporting_arm_1'), 'msi_grace'] = str(msi_grace)
+    out.loc[(cid, 'reporting_arm_1'), 'chktot'] = str(chktot)
+    out.loc[(cid, 'reporting_arm_1'), 'misschk'] = str(msi_grace - chktot)
 
-project.import_records(to_import=out, import_format="json")
+project.import_records(to_import=out.reset_index(), import_format="df")
