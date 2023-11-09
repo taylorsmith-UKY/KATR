f989576aae4c2ad9c86b6b76cf313e89e999eb2e
Taylor Smith <taylor.smith2@uky.edu>
2023-11-09 14:16:55 -0500

    Cleaned up dataframe creation, fixed dataframe value assignment, and added inventory management fields

--- update_client_progress.py
+++ update_client_progress.py
@@ -8,41 +8,44 @@ with open("api_key.txt", "r") as f:
     api_key = f.readline().strip()
 project = Project(api_url, api_key)
 date_format = "%Y-%m-%d"
+out_event = 'reporting_arm_1'
 
-# Fields to get from REDCap
-fields = ['idate', 'name', 'phone', 'coord']
+# Import parameters from REDCap
+fields = ['idate', 'name', 'phone', 'coord', 'addr', 'dob']
 events = ['intake_arm_1', "discharge_arm_1", "3month_postdischar_arm_1", "12month_postintake_arm_1"]
-
 data = project.export_records(format_type="df", fields=fields, raw_or_label="raw", events=events).drop(
     ['redcap_repeat_instrument', 'redcap_repeat_instance'], axis=1)
 
-# Create new dataframe for new report values
+# New dataframe components for new report values
 ids = data.index.get_level_values(0).unique()
 nids = len(ids)
-new = pd.DataFrame(dict(dname=[""]*nids, dphone=[""]*nids, comp_status=[""]*nids, first_idate=[""]*nids,
-                        last_idate=[""]*nids, next_idate=[""]*nids, dcoord=[""]*nids,
-                        days_since_last=[""]*nids, days_to_next=[""]*nids),
-                   index=pd.MultiIndex.from_product([ids, ['reporting_arm_1']], names=['cid', 'redcap_event_name']))
+columns = ['dname', 'rname', 'dphone', 'comp_status', 'first_idate', 'last_idate', 'last_int',
+           'next_idate', 'dcoord', 'days_since_last', 'days_to_next', 'addr2', 'dob_inv', 'bslncomp']
+index = pd.MultiIndex.from_product([ids, [out_event]], names=['cid', 'redcap_event_name'])
+values = [[""] * len(columns)] * nids
+new = pd.DataFrame(data=values, columns=columns, index=index)
 
 for uid in ids:
     # Exclude records with no valid intake data
     if events[0] not in data.loc[uid].index.values:
-        new = new.drop(uid)
+        new = new.drop(uid, level=0)
         continue
     elif data['idate'][uid][events[0]] == "":
         print(f"Client {uid} not being processed due to missing intake date.")
-        new = new.drop(uid)
+        new = new.drop(uid, level=0)
         continue
 
     intake = data['idate'][uid]['intake_arm_1']
     dname = data['name'][uid]['intake_arm_1']
     dcoord = data['coord'][uid]['intake_arm_1']
+    dob_inv = data['dob'][uid]['intake_arm_1']
 
     comp_stat = len(data.loc[uid])
     last_event = events[len(data.loc[uid]) - 1]
-    dphone = data['phone'][uid][last_event]
+    dphone = data['phone'][uid][last_event]         # use last event in case updated since intake
+    addr2 = data['addr'][uid][last_event]
 
-    last_idate = data['idate'][uid][last_event]
+    last_idate = dt.strptime(data['idate'][uid][last_event], date_format)
     ndlast = (dt.today() - last_idate).days
 
     if comp_stat == 1:                              # Completed Intake
@@ -54,17 +57,22 @@ for uid in ids:
     elif comp_stat == 3:                            # Completed 3-month Post Discharge Follow-up
         dnext = intake + rdelt(months=12)
         ndnext = (dnext - dt.today()).days
-    elif comp_stat == 4:                            # Completed 12-month Post Intake Follow-up
+    else:                            # Completed 12-month Post Intake Follow-up
         dnext = ""
         ndnext = ""
 
-    new['comp_status'][uid]['report_arm_1'] = comp_stat
-    new['dname'][uid]['report_arm_1'] = dname
-    new['dphone'][uid]['report_arm_1'] = dphone
-    new['first_idate'][uid]['report_arm_1'] = dt.strftime(intake, date_format)
-    new['last_idate'][uid]['report_arm_1'] = dt.strftime(last_idate, date_format)
-    new['next_idate'][uid]['report_arm_1'] = dt.strftime(dnext, date_format)
-    new['days_since_last'][uid]['report_arm_1'] = ndlast
-    new['days_to_next'][uid]['report_arm_1'] = ndnext
+    new.loc[(uid, out_event), 'comp_status'] = comp_stat
+    new.loc[(uid, out_event), 'dname'] = dname
+    new.loc[(uid, out_event), 'rname'] = dname
+    new.loc[(uid, out_event), 'dphone'] = dphone
+    new.loc[(uid, out_event), 'addr2'] = addr2
+    new.loc[(uid, out_event), 'dob_inv'] = dob_inv
+    new.loc[(uid, out_event), 'first_idate'] = dt.strftime(intake, date_format)
+    new.loc[(uid, out_event), 'bsln_comp'] = dt.strftime(intake, date_format)
+    new.loc[(uid, out_event), 'last_idate'] = dt.strftime(last_idate, date_format)
+    new.loc[(uid, out_event), 'last_int'] = dt.strftime(last_idate, date_format)
+    new.loc[(uid, out_event), 'next_idate'] = dt.strftime(dnext, date_format)
+    new.loc[(uid, out_event), 'days_since_last'] = ndlast
+    new.loc[(uid, out_event), 'days_to_next'] = ndnext
 
-project.import_records(to_import=new, import_format="df")
+project.import_records(to_import=new.reset_index(), import_format="df")
