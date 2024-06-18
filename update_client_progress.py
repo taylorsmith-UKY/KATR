7e0ae2cab132170d07d877ff0e8a8437620cfa68
Taylor Smith <taylor.smith2@uky.edu>
2024-06-18 18:56:57 -0400

    Add calculation for months since last contact

--- update_client_progress.py
+++ update_client_progress.py
@@ -3,6 +3,7 @@ from redcap import Project
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta as rdelt
 import numpy as np
+import json
 
 api_url = 'https://redcap.uky.edu/redcap/api/'
 with open("api_key.txt", "r") as f:
@@ -10,6 +11,14 @@ with open("api_key.txt", "r") as f:
 project = Project(api_url, api_key)
 date_format = "%Y-%m-%d"
 out_event = 'reporting_arm_1'
+with open("linkages.json", 'r') as f:
+    link = json.load(f)
+
+# Inventory management data
+fields_inv = ['cardmail', 'clicont', 'cloadint', 'cloaddis', 'cload3mo', 'cload12m']
+inv_data = project.export_records(format_type="df", fields=fields_inv, raw_or_label="raw", events=["reporting_arm_1"])\
+    .drop(['redcap_repeat_instrument', 'redcap_repeat_instance'], axis=1)
+inv_data.index = inv_data.index.get_level_values(0)
 
 # Import interview data from REDCap
 fields = ['idate', 'name', 'phone', 'coord', 'addr', 'dob', 'addr', 'disch_st', 'cty', 'age', 'gender', 'email',
@@ -17,15 +26,13 @@ fields = ['idate', 'name', 'phone', 'coord', 'addr', 'dob', 'addr', 'disch_st', 
           'bneeds_v', 'sl_v', 'sobliv_b', 'sobliv_b_mo1_np', 'sobliv_b_mo2_np', 'sobliv_b_mo3_np', 'sobliv_b_mo4_np',
           'sobliv_b_mo5_np', 'sobliv_b_mo1_p', 'sobliv_b_mo2_p', 'sobliv_b_mo3_p', 'sobliv_b_mo4_p', 'sobliv_b_mo5_p',
           'trans_b', 'trans_v', 'trans_t', 'employ_b', 'employ_v', 'employ_t']
+
 events = ['intake_arm_1', "discharge_arm_1", "3month_postdischar_arm_1", "12month_postintake_arm_1"]
 data = project.export_records(format_type="df", fields=fields, raw_or_label="raw", events=events)\
     .drop(['redcap_repeat_instrument', 'redcap_repeat_instance'], axis=1)
 
-# Inventory management data
-fields_inv = ['cardmail', 'clicont', 'cloadint', 'cloaddis', 'cload3mo', 'cload12m']
-inv_data = project.export_records(format_type="df", fields=fields_inv, raw_or_label="raw", events=["reporting_arm_1"])\
-    .drop(['redcap_repeat_instrument', 'redcap_repeat_instance'], axis=1)
-inv_data.index = inv_data.index.get_level_values(0)
+# Import monthly check-in data (for months since last contact)
+chdata = project.export_records(format_type="df", fields=["chkdate"], events=["monthy_checkin_arm_1"])
 
 # Generate list of column names for multiple-selection fields
 plnserv_cols = [x for x in data.columns if "plnserv___" in x and "____" not in x]
@@ -46,8 +53,11 @@ diag_cols = {'alcohol': [x for x in data.columns if "almeds___" in x and "____" 
              'tobacco': [x for x in data.columns if "tomeds___" in x and "____" not in x][-1]}
 
 sobliv_cols = ['sobliv_b_mo1', 'sobliv_b_mo2', 'sobliv_b_mo3', 'sobliv_b_mo4', 'sobliv_b_mo5']
+sobliv_cols_np = [x + "_np" for x in sobliv_cols]
+sobliv_cols_p = [x + "_p" for x in sobliv_cols]
 
 sud_cols = [f"sud_diag___{i+1}" for i in range(4)]
+
 # New dataframe components for new report values
 ids = data.index.get_level_values(0).unique()
 nids = len(ids)
@@ -56,23 +66,28 @@ columns = ['dcoord', 'dname', 'comp_status', 'first_idate', 'disch_idate', 'mo3p
            'dphone', 'dob_inv', 'bslncomp', 'last_int', 'serv_reg', 'gender_2'] + \
            bel_pop_2_cols + race_2_cols + eth_2_cols + sud_cols + sobliv_cols + \
           ['bneeds_b_2', 'bneeds_v_2', 'sobliv_b_2', 'sl_v_2', 'trans_b_2', 'trans_v_2', 'trans_t_2',
-           'employ_b_2', 'employ_v_2', 'employ_t_2', 'age_2']
+           'employ_b_2', 'employ_v_2', 'employ_t_2', 'age_2', 'mo_last_cont']
 
 index = pd.MultiIndex.from_product([ids, [out_event]], names=['cid', 'redcap_event_name'])
-values = [[-1] * len(columns)] * nids
-new = pd.DataFrame(data=values, columns=columns, index=index)
-# for col in plnserv_2_cols:
-#     new[col] = np.zeros(len(new), dtype=int)
-for col in bel_pop_2_cols:
-    new[col] = np.zeros(len(new), dtype=int)
-for col in race_2_cols:
-    new[col] = np.zeros(len(new), dtype=int)
-for col in eth_2_cols:
-    new[col] = np.zeros(len(new), dtype=int)
-for col in sud_cols:
-    new[col] = np.zeros(len(new), dtype=int)
+new = pd.DataFrame(columns=columns, index=index)
+new[bel_pop_2_cols] = new[bel_pop_2_cols].fillna(0).astype("int64")
+new[race_2_cols] = new[race_2_cols].fillna(0).astype("int64")
+new[eth_2_cols] = new[eth_2_cols].fillna(0).astype("int64")
+new[sud_cols] = new[sud_cols].fillna(0).astype("int64")
 new['sobliv_b_2'] = np.zeros(len(new), dtype=int)
 new['age_2'] = np.zeros(len(new), dtype=int)
+
+data[bel_pop_cols] = data[bel_pop_cols].fillna(0).astype("int64")
+data[race_cols] = data[race_cols].fillna(0).astype("int64")
+data[eth_cols] = data[eth_cols].fillna(0).astype("int64")
+data[sobliv_cols_p] = data[sobliv_cols_p].fillna(0).astype("int64")
+data[sobliv_cols_np] = data[sobliv_cols_np].fillna(0).astype("int64")
+data["sobliv_b"] = data["sobliv_b"].fillna(0).astype("int64")
+data["bneeds_b"] = data["bneeds_b"].fillna(0).astype("int64")
+data["trans_b"] = data["trans_b"].fillna(0).astype("int64")
+data["employ_b"] = data["employ_b"].fillna(0).astype("int64")
+data["gender"] = data["gender"].fillna(-1).astype("int64")
+data["age"] = data["age"].fillna(-1).astype("int64")
 
 for uid in ids:
     # Exclude clients with no valid intake data
@@ -81,48 +96,35 @@ for uid in ids:
         new = new.drop(uid, level=0)
         continue
 
-    intake = data['idate'][uid]['intake_arm_1']     # For use calculating next interview deadline
+    client = data.loc[uid]                                  # Extract individual client data
+
+    intake = client['idate'][events[0]]                     # For use calculating next interview deadline
     intake_dt = dt.strptime(intake, date_format)
-    comp_stat = 0
-    for event in data.loc[uid].index:
-        if type(data['idate'][uid][event]) == str and data['idate'][uid][event] != "":
-            comp_stat += 1
-    last_event = events[comp_stat - 1]
 
-    last_idate = dt.strptime(data['idate'][uid][last_event], date_format)
-    ndlast = (dt.today() - last_idate).days
-
-    if data.loc[(uid, events[0]), diag_cols["alcohol"]] == 0:
+    # Determine SUD substances
+    if client.loc[events[0], diag_cols["alcohol"]] == 0:
         new.loc[(uid, out_event), 'sud_diag___1'] = 1
-    if data.loc[(uid, events[0]), diag_cols["opioids"]] == 0:
+    if client.loc[events[0], diag_cols["opioids"]] == 0:
         new.loc[(uid, out_event), 'sud_diag___2'] = 1
-    if data.loc[(uid, events[0]), diag_cols["stimulants"]] == 0:
+    if client.loc[events[0], diag_cols["stimulants"]] == 0:
         new.loc[(uid, out_event), 'sud_diag___3'] = 1
 
-    # for i in range(len(plnserv_cols)):
-    #     if data.loc[(uid, events[0]), plnserv_cols[i]] == 1:
-    #         new.loc[(uid, out_event), plnserv_2_cols[i]] = 1
-    for i in range(len(bel_pop_cols)):
-        if data.loc[(uid, events[0]), bel_pop_cols[i]] == 1:
-            new.loc[(uid, out_event), bel_pop_2_cols[i]] = 1
-    for i in range(len(race_cols)):
-        if data.loc[(uid, events[0]), race_cols[i]] == 1:
-            new.loc[(uid, out_event), race_2_cols[i]] = 1
-    for i in range(len(eth_cols)):
-        if data.loc[(uid, events[0]), eth_cols[i]] == 1:
-            new.loc[(uid, out_event), eth_2_cols[i]] = 1
+    # Copy priority population, race, and ethnicity columns for reporting event
+    new.loc[(uid, out_event), bel_pop_2_cols] = client.loc[events[0], bel_pop_cols]
+    new.loc[(uid, out_event), race_2_cols] = client.loc[events[0], race_cols]
+    new.loc[(uid, out_event), eth_2_cols] = client.loc[events[0], eth_cols]
 
+    # For keeping track of incentive dispersals
     needsend = 0
     needload = 0
-    serv_reg = 0
-    if data['cty'][uid][last_event] in [5, 8, 12, 13, 14, 16, 18, 19, 22]:
-        serv_reg = 1
-    elif data['cty'][uid][last_event] in [2, 9, 10, 15, 20, 21]:
-        serv_reg = 2
-    elif data['cty'][uid][last_event] in [1, 3, 4, 6, 7, 11, 17]:
-        serv_reg = 3
 
-    if comp_stat == 1:                              # Completed Intake
+    # Determine the latest interview and determine when the target for the next interview date
+    comp_stat = 0
+    # Completed Intake
+    if events[1] not in client.index or (pd.isna(client['idate'][events[1]]) or client['idate'][events[1]] == ""):
+        comp_stat = 1
+        last_event = events[0]
+        last_idate = dt.strptime(client['idate'][last_event], date_format)
         dnext = last_idate + rdelt(months=6)
         ndnext = (dnext - dt.today()).days
         if uid not in inv_data.index.values or inv_data.loc[uid, "cardmail"] == "" or \
@@ -133,37 +135,59 @@ for uid in ids:
                 type(inv_data.loc[uid, "cloadint"]) == float or inv_data.loc[uid, "cloadint"] == "":
             needload = 1
 
-    elif comp_stat == 2:                            # Completed Discharge
+    # Completed Discharge
+    elif events[2] not in client.index or (pd.isna(client['idate'][events[2]]) or client['idate'][events[2]] == ""):
+        comp_stat = 2
+        last_event = events[1]
+        last_idate = dt.strptime(client['idate'][last_event], date_format)
         dnext = intake_dt + rdelt(months=6)
         ndnext = (dnext - dt.today()).days
         if inv_data.loc[uid, "cloaddis"] == "" or type(inv_data.loc[uid, "cloaddis"]) == float:
             needload = 1
-        if data['disch_st'][uid][events[1]] == 2:
+        if client['disch_st'][events[1]] == 2:
             comp_stat = 10
-    elif comp_stat == 3:                            # Completed 3-month Post Discharge Follow-up
+        elif client['disch_st'][events[1]] == 3:
+            comp_stat = 11
+    # Completed 3-month Post Discharge Follow-up
+    elif events[3] not in client.index or (pd.isna(client['idate'][events[3]]) or client['idate'][events[3]] == ""):
+        comp_stat = 3
+        last_event = events[2]
+        last_idate = dt.strptime(client['idate'][last_event], date_format)
         dnext = intake_dt + rdelt(months=12)
         ndnext = (dnext - dt.today()).days
         if inv_data.loc[uid, "cload3mo"] == "" or type(inv_data.loc[uid, "cload3mo"]) == float:
             needload = 1
-    else:                            # Completed 12-month Post Intake Follow-up
+    # Completed 12-month Post Intake Follow-up
+    else:
+        comp_stat = 4
+        last_event = events[3]
+        last_idate = dt.strptime(client['idate'][last_event], date_format)
         dnext = ""
         ndnext = ""
         if inv_data.loc[uid, "cload12m"] == "" or type(inv_data.loc[uid, "cload12m"]) == float:
             needload = 1
+    ndlast = (dt.today() - last_idate).days
 
-    # Sober living budget
+    lcont = last_idate
+    if uid in chdata.index.get_level_values(0):
+        for tdate in chdata['chkdate'][uid]:
+            lcont = max(lcont, dt.strptime(tdate, date_format))
+
+    mslc = (dt.today().year - lcont.year) * 12 + dt.today().month - lcont.month
+
+    # Service region (serv_reg)
+    serv_reg = 0
+    for reg in link['region']['groups']:
+        if client[link['region']['fieldname']][last_event] in link['region']['groups'][reg]['matches']:
+            serv_reg = link['region']['groups'][reg]['value']
+
+    # Sober living budget (select values based on whether pregnant/post-partum or has minors
     sobliv = []
     for i in range(1, 6):
         if data.loc[(uid, events[0]), 'preppmin'] == 1:
-            try:
-                sobliv.append(int(data.loc[(uid, events[0]), f'sobliv_b_mo{i}_p']))
-            except ValueError:
-                sobliv.append(0)
+            sobliv.append(int(client.loc[events[0], f'sobliv_b_mo{i}_p']))
         else:
-            try:
-                sobliv.append(int(data.loc[(uid, events[0]), f'sobliv_b_mo{i}_np']))
-            except ValueError:
-                sobliv.append(0)
+            sobliv.append(int(client.loc[events[0], f'sobliv_b_mo{i}_np']))
 
     # Updates for Program Completion and Inventory Management Instruments in REDCap
     new.loc[(uid, out_event), 'comp_status'] = comp_stat
@@ -175,53 +199,35 @@ for uid in ids:
     new.loc[(uid, out_event), 'needload'] = needload
 
     # Copy values to corresponding instruments for convenience when viewing in REDCap
-    new.loc[(uid, out_event), 'dname'] = data['name'][uid]['intake_arm_1']          # Program Completion
-    new.loc[(uid, out_event), 'dcoord'] = data['coord'][uid]['intake_arm_1']        # Program Completion
+    new.loc[(uid, out_event), 'dname'] = client['name']['intake_arm_1']          # Program Completion
+    new.loc[(uid, out_event), 'dcoord'] = client['coord']['intake_arm_1']        # Program Completion
     new.loc[(uid, out_event), 'first_idate'] = intake                               # Program Completion
     new.loc[(uid, out_event), 'serv_reg'] = serv_reg                                # Program Completion
+    new.loc[(uid, out_event), 'mo_last_cont'] = mslc                             # Program Completion
 
-    new.loc[(uid, out_event), 'rname'] = data['name'][uid]['intake_arm_1']           # Inventory Management
-    new.loc[(uid, out_event), 'dphone'] = data['phone'][uid][last_event]             # Inventory Management
-    new.loc[(uid, out_event), 'addr2'] = data['addr'][uid][last_event]               # Inventory Management
+    new.loc[(uid, out_event), 'rname'] = client['name']['intake_arm_1']           # Inventory Management
+    new.loc[(uid, out_event), 'dphone'] = client['phone'][last_event]             # Inventory Management
+    new.loc[(uid, out_event), 'addr2'] = client['addr'][last_event]               # Inventory Management
 
-    new.loc[(uid, out_event), 'demail'] = data['email'][uid][last_event]             # Recruitment Info
-    try:
-        new.loc[(uid, out_event), 'age_2'] = int(data['age'][uid][events[0]])                 # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'age_2'] = ""
-    try:
-        new.loc[(uid, out_event), 'bneeds_b_2'] = int(data['bneeds_b'][uid][events[0]])  # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'bneeds_b_2'] = 0
-    new.loc[(uid, out_event), 'bneeds_v_2'] = data['bneeds_v'][uid][events[0]]
-    try:
-        new.loc[(uid, out_event), 'sobliv_b_2'] = int(data['sobliv_b'][uid][events[0]])      # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'sobliv_b_2'] = 0
-    new.loc[(uid, out_event), 'sl_v_2'] = data['sl_v'][uid][events[0]]        # Recruitment Info
+    new.loc[(uid, out_event), 'demail'] = client['email'][last_event]             # Recruitment Info
+    new.loc[(uid, out_event), 'age_2'] = client['age'][events[0]]                 # Recruitment Info
+    new.loc[(uid, out_event), 'bneeds_b_2'] = client['bneeds_b'][events[0]]  # Recruitment Info
+    new.loc[(uid, out_event), 'bneeds_v_2'] = client['bneeds_v'][events[0]]
+    new.loc[(uid, out_event), 'sobliv_b_2'] = client['sobliv_b'][events[0]]      # Recruitment Info
+    new.loc[(uid, out_event), 'sl_v_2'] = client['sl_v'][events[0]]        # Recruitment Info
     new.loc[(uid, out_event), 'sobliv_b_mo1'] = sobliv[0]                            # Recruitment Info
     new.loc[(uid, out_event), 'sobliv_b_mo2'] = sobliv[1]                            # Recruitment Info
     new.loc[(uid, out_event), 'sobliv_b_mo3'] = sobliv[2]                            # Recruitment Info
     new.loc[(uid, out_event), 'sobliv_b_mo4'] = sobliv[3]                            # Recruitment Info
     new.loc[(uid, out_event), 'sobliv_b_mo5'] = sobliv[4]                            # Recruitment Info
-    try:
-        new.loc[(uid, out_event), 'trans_b_2'] = int(data['trans_b'][uid][events[0]])    # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'trans_b_2'] = 0
-    new.loc[(uid, out_event), 'trans_v_2'] = data['trans_v'][uid][events[0]]    # Recruitment Info
-    new.loc[(uid, out_event), 'trans_t_2'] = data['trans_t'][uid][events[0]]    # Recruitment Info
-    try:
-        new.loc[(uid, out_event), 'employ_b_2'] = int(data['employ_b'][uid][events[0]])  # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'employ_b_2'] = 0
-    new.loc[(uid, out_event), 'employ_v_2'] = data['employ_v'][uid][events[0]]  # Recruitment Info
-    new.loc[(uid, out_event), 'employ_t_2'] = data['employ_t'][uid][events[0]]  # Recruitment Info
-    try:
-        new.loc[(uid, out_event), 'gender_2'] = int(data['gender'][uid][events[0]])          # Recruitment Info
-    except ValueError:
-        new.loc[(uid, out_event), 'gender_2'] = -1
-
-    new.loc[(uid, out_event), 'dob_inv'] = data['dob'][uid]['intake_arm_1']         # Inventory Management
+    new.loc[(uid, out_event), 'trans_b_2'] = client['trans_b'][events[0]]    # Recruitment Info
+    new.loc[(uid, out_event), 'trans_v_2'] = client['trans_v'][events[0]]    # Recruitment Info
+    new.loc[(uid, out_event), 'trans_t_2'] = client['trans_t'][events[0]]    # Recruitment Info
+    new.loc[(uid, out_event), 'employ_b_2'] = client['employ_b'][events[0]]  # Recruitment Info
+    new.loc[(uid, out_event), 'employ_v_2'] = client['employ_v'][events[0]]  # Recruitment Info
+    new.loc[(uid, out_event), 'employ_t_2'] = client['employ_t'][events[0]]  # Recruitment Info
+    new.loc[(uid, out_event), 'gender_2'] = client['gender'][events[0]]          # Recruitment Info
+    new.loc[(uid, out_event), 'dob_inv'] = client['dob']['intake_arm_1']         # Inventory Management
     new.loc[(uid, out_event), 'last_int'] = dt.strftime(last_idate, date_format)    # Inventory Management
 
 project.import_records(to_import=new.reset_index(), import_format="df", date_format="YMD")
